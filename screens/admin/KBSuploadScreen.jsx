665c01efedfe17878d70dcb09dee3394077e94be
sam2002jain <jainsam1975@gmail.com>
2025-09-15 10:35:36 +0530

    added admin folder

---
+++ screens/admin/KBSuploadScreen.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const KBSuploadScreen = () => {
+  return (
+    <View>
+      <Text>KBSuploadScreen</Text>
+    </View>
+  )
+}
+
+export default KBSuploadScreen
+
+const styles = StyleSheet.create({})
