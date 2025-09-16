e92b561d9077b507a338bc3234127bc0768b7ce0
sam2002jain <jainsam1975@gmail.com>
2025-09-16 16:59:39 +0530

    upload screen isready

--- screens/admin/KBSuploadScreen.jsx
+++ screens/admin/KBSuploadScreen.jsx
@@ -1,14 +1,55 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 const KBSuploadScreen = () => {
   return (
-    <View>
-      <Text>KBSuploadScreen</Text>
-    </View>
+    <SafeAreaView>
+      <View style={styles.header}>
+        <Text style={styles.headertext}>
+            Upload KBS Question Data in Excel file
+        </Text>
+      </View>
+
+      <View style={styles.maincontainer}>
+        <TouchableOpacity style={styles.uploadbtn}>
+          <Text style={styles.uploadbtntxt}>Upload file</Text>
+        </TouchableOpacity>
+
+        <View>
+          {/* //here we can show success and error message// */}
+
+          
+        </View>
+      </View>
+
+
+    </SafeAreaView>
   )
 }
 
 export default KBSuploadScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  header:{
+    flex:1,
+    justifyContent:'center',
+    alignContent:'center'
+  },
+  headertext:{
+    textAlign:'center',
+    fontSize:18,
+    fontWeight:'600,'
+  },
+  maincontainer:{
+    justifyContent:'center',
+    flex:1,
+    flexDirection:'column'
+  },
+  uploadbtn:{
+
+  },
+  uploadbtntxt:{
+
+  }
+,})
